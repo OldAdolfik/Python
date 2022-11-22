daa3d4823b745945a743f764a42161ec95db0a56
Stepan <apollo.black166@gmail.com>
2022-11-22 18:46:20 +0300

    create a new rep for python

---
+++ HelloPython/lec.py
@@ -0,0 +1,30 @@
+# print('Hello world!')
+
+# value = None
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+# value = 12334
+# # print(type(value))
+# s = "hello world"
+# print(s)
+# print(f'{a} - {b} - {s}')
+# f = True
+# print(f)
+# list = [1,2,3]
+# print(list)
+# a =1.3
+# b =3
+# c=round(a*b,5)
+# print(c)
+
+# a = [1,2,3,4,5]
+
+# print(2 in a)
+a = int(input('a = '))
+b = int(input('b = '))
+if a>b:
+    print(a)
+else:
+    print(b)
